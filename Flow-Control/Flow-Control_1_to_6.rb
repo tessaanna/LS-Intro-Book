e8ab5c7a013540a85f2a28b7d16a337a651d6b77
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-06 14:37:43 -0400

    finished flow control exercises

--- Flow-Control/Flow-Control_1_to_6.rb
+++ Flow-Control/Flow-Control_1_to_6.rb
@@ -28,16 +28,66 @@ ques_two("tantamount!")
 ques_two("hello")
 
 puts ""
-puts "Question 3"
-puts "Give me a number between 0 and 100"
-number = gets.chomp
-number = number.to_i
-case number
-when 0..50 then puts "between 0 and 50"
-when 51..100 then puts "between 51 and 100"
-else
-    puts "That number is not between 0 and 100"
+puts "Question 3 & 5"
+
+def number_choice
+  puts "Give me a number between 0 and 100"
+  number = gets.chomp
+  number = number.to_i
+  $number = number
 end
+
+def case_reply(num)
+  case num
+  when 0..50 then puts "between 0 and 50"
+  when 51..100 then puts "between 51 and 100"
+  else
+    puts "That number is not between 0 and 100"
+  end
+end
+
+number_choice
+case_reply($number)
 
 puts ""
 puts "Question 4"
+
+puts "1"
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+puts false
+
+puts "2"
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+puts "Did you get it right?"
+
+puts "3"
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright."
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+puts "Alright now!"
+
+puts ""
+puts "Question 6"
+
+puts "Error due to there not being an end at end of method"
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+equal_to_four(5)

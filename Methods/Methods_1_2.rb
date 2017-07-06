be322e3f74f88321c4843042f12c8de33bafd2e6
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-06 12:53:53 -0400

    Updates to Methods exercises

---
+++ Methods/Methods_1_2.rb
@@ -0,0 +1,29 @@
+puts "question 1"
+def greeting(name)
+  puts "Hello! #{name}"
+end
+
+puts "What is your name?"
+name_1 = gets.chomp
+greeting(name_1)
+
+puts ""
+puts "question 2"
+puts "1:"
+x = 2
+
+puts "2:"
+puts x = 2
+puts "prints nil"
+
+puts "3:"
+p name = "Joe"
+puts "prints Joe"
+
+puts "4:"
+four = "four"
+puts "prints nil"
+
+puts "5:"
+print something = "nothing"
+puts "prints nil"

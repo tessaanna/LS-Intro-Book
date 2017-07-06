033b1c9f3f47f219285b2b5354f9bb055e216729
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-06 13:46:14 -0400

    More Answers Flow Control

--- Flow-Control/Flow-Control_1_to_6.rb
+++ Flow-Control/Flow-Control_1_to_6.rb
@@ -17,3 +17,27 @@ puts true
 
 puts ""
 puts "Question 2"
+def ques_two(string_1)
+  if string_1.length > 10
+    puts string_1.upcase
+  else
+    puts "string isn't long enough"
+  end
+end
+ques_two("tantamount!")
+ques_two("hello")
+
+puts ""
+puts "Question 3"
+puts "Give me a number between 0 and 100"
+number = gets.chomp
+number = number.to_i
+case number
+when 0..50 then puts "between 0 and 50"
+when 51..100 then puts "between 51 and 100"
+else
+    puts "That number is not between 0 and 100"
+end
+
+puts ""
+puts "Question 4"

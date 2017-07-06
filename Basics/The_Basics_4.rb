34502be4d08f5ec4e657b813bf19717def28033a
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-06 12:24:23 -0400

    Fixes to Basics

--- Basics/The_Basics_4.rb
+++ Basics/The_Basics_4.rb
@@ -1,13 +1,13 @@
 movie_dates = [2013, 2014, 2015, 2016, 2017]
 
 movie_dates.each do |x|
-  put x
+  puts x
 end
 
 #or
 
-movie_dates[0]
-movie_dates[1]
-movie_dates[2]
-movie_dates[3]
-movie_dates[4]
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]

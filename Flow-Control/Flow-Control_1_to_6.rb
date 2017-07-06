22ce802b2cae52ee45734e0615d7df420410ecc9
Teresa DAnna <teresadanna@Teresas-MacBook-Pro.local>
2017-07-06 13:31:02 -0400

    Flow Control Folder

---
+++ Flow-Control/Flow-Control_1_to_6.rb
@@ -0,0 +1,19 @@
+puts "Question 1"
+puts "1:"
+puts (32 * 4) >= 129
+puts false
+puts "2:"
+puts false != !true
+puts false
+puts "3:"
+puts true == 4
+puts false
+puts "4:"
+puts false == (847 == '847')
+puts true
+puts "5:"
+puts (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+puts true
+
+puts ""
+puts "Question 2"
